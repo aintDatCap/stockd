7bdb395a7e03e1acfa0291a6141e1d77ed5df605
yrenum <89939707+yrenum@users.noreply.github.com>
2023-06-29 20:02:20 +0200

    Created the setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='stockazzo',
+      version='alpha',
+      description='Library to simulate a trading enviroment',
+      author='Federico Caprini',
+      author_email='federico.caprini@protonmail.com',
+      url='github.com/yrenum/stockazzo',
+      packages=['stockazzo'],
+      )
