05be94824824425ad0f2472a34b3b8550a7d1c67
yrenum <89939707+yrenum@users.noreply.github.com>
2023-10-22 16:07:41 +0200

    Changed from requires to install_requires

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from distutils.core import setup
 
 setup(name='stockazzo',
-      version='alpha',
+      version='0.1-alpha',
       description='A library to simulate a trading environment',
       author='Federico Caprini',
       author_email='federico.caprini@protonmail.com',
       url='github.com/yrenum/stockazzo',
       packages=['stockazzo'],
-      requires=["stockholm",
-                "gymnasium",
-                "pandas",
-                "numpy",
-                "dask",
-                "pandas-ta"],
+      install_requires=["stockholm",
+                        "gymnasium",
+                        "pandas",
+                        "numpy",
+                        "dask",
+                        "pandas-ta"],
       )
