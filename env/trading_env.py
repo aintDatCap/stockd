d50e209317cb5cbc9220eee3d260b7417dea5402
yrenum <89939707+yrenum@users.noreply.github.com>
2023-10-28 14:47:19 +0200

    Fixed a little bug

--- env/trading_env.py
+++ env/trading_env.py
@@ -195,26 +195,26 @@ class TradingEnv(gym.Env):
 
         if self.__get_pl_percentage() <= -0.3:
             reward += self._close_positions()
+        else:
+            if action == Action.Buy:
+                if self.__stock["type"] == Position.Short and self.__stock["qty"] != 0:
+                    reward += self._close_positions()
+                else:
+                    self._buy(Rate(0.02))
+                    reward += 0.2
 
-        if action == Action.Buy:
-            if self.__stock["type"] == Position.Short and self.__stock["qty"] != 0:
+            elif action == Action.Sell:
+                if self.__stock["type"] == Position.Long and self.__stock["qty"] != 0:
+                    reward += self._close_positions()
+                else:
+                    self._sell(Rate(0.02))
+                    reward += 0.2
+
+            self.__current_row += 1
+            terminated = self.__current_row == len(self.__dataframe.index) - 1
+            if terminated:
                 reward += self._close_positions()
-            else:
-                self._buy(Rate(0.02))
-                reward += 0.2
-
-        elif action == Action.Sell:
-            if self.__stock["type"] == Position.Long and self.__stock["qty"] != 0:
-                reward += self._close_positions()
-            else:
-                self._sell(Rate(0.02))
-                reward += 0.2
-
-        self.__current_row += 1
-        terminated = self.__current_row == len(self.__dataframe.index) - 1
-        if terminated:
-            reward += self._close_positions()
-            print(f"Start: {self.__starting_equity}$, end: {self.__get_current_equity()}$")
+                print(f"Start: {self.__starting_equity}$, end: {self.__get_current_equity()}$")
 
         return self._get_obs(), reward, terminated, False, self._get_info()
 
