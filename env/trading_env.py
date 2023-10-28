2fffad300f742569fda5af090b4ad2265870c91a
yrenum <89939707+yrenum@users.noreply.github.com>
2023-10-28 12:14:10 +0200

    Nothing important

--- env/trading_env.py
+++ env/trading_env.py
@@ -213,9 +213,8 @@ class TradingEnv(gym.Env):
         self.__current_row += 1
         terminated = self.__current_row == len(self.__dataframe.index) - 1
         if terminated:
-            print(f"Start: {self.__starting_equity}$, end: {self.__get_current_equity()}$")
-
             reward += self._close_positions()
+            print(f"Start: {self.__starting_equity}$, end: {self.__get_current_equity()}$")
 
         return self._get_obs(), reward, terminated, False, self._get_info()
 
